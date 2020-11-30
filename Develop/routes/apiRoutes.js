5b3e4555791ea89db70ae450f37b7087fb1cba11
Travis Mendenhall <twmwayne@gmail.com>
2020-11-30 13:07:13 -0700

    added get and post methods

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -16,3 +16,33 @@ module.exports = function(app){
             }
         });
     }
+
+    //========== API ROUTES ==========
+
+    // GET Method to return all notes
+    app.get("/api/notes", function(req, res){
+        res.json(notesData);
+    });
+
+    // POST Method to add notes
+    app.post("/api/notes", function(req, res){
+
+        // Set unique id to entry
+        if (notesData.length == 0){
+            req.body.id = "0";
+        } else{
+            req.body.id = JSON.stringify(JSON.parse(notesData[notesData.length - 1].id) + 1);
+        }
+        
+        console.log("req.body.id: " + req.body.id);
+
+        // Pushes Body to JSON Array
+        notesData.push(req.body);
+
+        // Write notes data to database
+        writeToDB(notesData);
+        console.log(notesData);
+
+        // returns new note in JSON format.
+        res.json(req.body);
+    });
