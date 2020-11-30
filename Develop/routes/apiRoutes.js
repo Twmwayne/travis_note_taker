0f2dc241e97177b00d18f396a952e6a95aea9977
Travis Mendenhall <twmwayne@gmail.com>
2020-11-30 12:59:46 -0700

    added functions to apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,4 +2,17 @@
 const fs = require("fs");
 const notesData = require("../db/db.json");
 
-module.exports = function(app)
+module.exports = function(app){
+
+    //========== FUNCTIONS ==========
+    function writeToDB(notes){
+        // Converts new JSON Array back to string
+        notes = JSON.stringify(notes);
+        console.log (notes);
+        // Writes String back to db.json
+        fs.writeFileSync("./db/db.json", notes, function(err){
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
